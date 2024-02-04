201646894b038c523c7cafcfa0b90878b8777706
delperdj <43620607+delperdj@users.noreply.github.com>
2024-02-04 01:00:31 +0000

    update menu file with qr code program

--- py_men_wrap.py
+++ py_men_wrap.py
@@ -20,7 +20,8 @@ def main():
         "2": ("Temperature Conversion", "py_temp_conv"),
         "3": ("Clipboard History", "py_clip_hist"),
         "4": ("Currency Conversion", "py_curr_conv"),
-        "5": ("Time Zone Conversion", "py_time_zone")
+        "5": ("Time Zone Conversion", "py_time_zone"),
+        "6": ("WiFi QR Code Generator", "py_wifi_qr.py")
     }
 
     while True:
